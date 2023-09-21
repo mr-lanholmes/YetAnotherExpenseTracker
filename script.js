4d5bfde328cef3f960ab7457791f0a5074726e24
mr-lanholmes <lan_rock@Live.com>
2023-09-21 21:59:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -24,6 +24,12 @@ function displayTasks() {
         return store.getAll();
     }).then(expenses => {
         console.log(expenses)
+        expenses.sort(function(a,b){
+            // Turn your strings into dates, and then subtract them
+            // to get a value that is either negative, positive, or zero.
+            return new Date(b.expenseInputDateTime) - new Date(a.expenseInputDateTime);
+        });
+        
         expenses.forEach(element => {
             let _tmp = ""
             _tmp += "<tr>"
